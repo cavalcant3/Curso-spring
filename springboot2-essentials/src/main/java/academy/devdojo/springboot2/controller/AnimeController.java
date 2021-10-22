6ba481d9584dfb9f5006138c75ff6390620dae24
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-10-22 13:16:51 -0300

    ajustes

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -2,27 +2,31 @@ package academy.devdojo.springboot2.controller;
 
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.serivce.ANimeService;
 import academy.devdojo.springboot2.util.DateUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController //isso é uma restapi
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
 
-    private DateUtil dateUtil;
-    //injeção de dependencias via constructor
-    public AnimeController(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
-    }
+    private final DateUtil dateUtil;
+    private final ANimeService animeService;
+
 
     //localhost:8080/anime/list
-    @GetMapping(path = "list") //endpoint .../list
+    @GetMapping //(path = "list") //endpoint .../list
     public List<Anime> list(){
-        return List.of(new Anime("DBZ"), new Anime("Bersek"));
+        log.info(dateUtil.formatLocalDateToDataBaseStyle(LocalDateTime.now()));
+        return animeService.listAll();
     }
 }
