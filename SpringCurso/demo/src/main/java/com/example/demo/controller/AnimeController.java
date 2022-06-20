7a784d3895144bf47bf4d27eb38892887fcbeb61
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-06-20 03:54:39 -0300

    Iniciando configuração

---
+++ SpringCurso/demo/src/main/java/com/example/demo/controller/AnimeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+
+public class AnimeController {
+
+}
